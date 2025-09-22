54b3bd86e7d14f3aae7142dd234854e39d85d2a6
Romaalie <bhd720@myy.haaga-helia.fi>
2025-09-22 11:12:48 +0000

    Initiate pipeline test

--- src/submit_run.py
+++ src/submit_run.py
@@ -32,4 +32,4 @@ if __name__ == "__main__":
 # Test commit 3 for pipeline. Last fix was not 1.8.2 but 1.8.3. Corrected
 # now.
 # Test commit 4 for pipeline. Changed git runner workflow to install kfp 1.8.2 also.
-
+# Test commit 5. Fix previous.
