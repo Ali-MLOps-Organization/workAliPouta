1dbd653236e9d43525dd6bd30fb90c45482d2ce9
Romaalie <bhd720@myy.haaga-helia.fi>
2025-09-23 05:04:54 +0000

    Test workflow

--- src/submit_run.py
+++ src/submit_run.py
@@ -37,3 +37,4 @@ if __name__ == "__main__":
 # Test commit 7. Change runner to utilize v4.
 # Test commit 8. Change runner python to 3.10.
 # Test commit 9. Revert to default python with v2.
+# Test commit 10. Revert to original pip install kfp~=1.8.14"
